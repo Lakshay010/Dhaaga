9c9c7b1fda970ace4e6d6752d5dd212b7c60460c
Lakshay010 <lakshaybhola0100@gmail.com>
2024-05-28 13:38:42 +0530

    Hopefully last commit

---
+++ server/Routes/userRoutes.js
@@ -0,0 +1,18 @@
+import Express from "express";
+import {signupUser,loginUser, logoutUser, followUnfollowUser, updateUser, getUserProfile}  from "../controllers/userController.js";
+import protectRoute from "../middleware/protectRoute.js";
+const router = Express.Router();
+
+router.get("/profile/:query",getUserProfile)
+router.post("/signup",signupUser)
+router.post("/login",loginUser)
+router.post("/logout",logoutUser)
+router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.put("/update/:id", protectRoute, updateUser);
+
+
+export default router;
+
+
+//Users ke routes hai ye
+                 
